0d0cb23372762bfefcae27f32162cb533e72d7cd
titik444 <momochi@yahoo.com>
2025-02-22 03:52:10 +0700

    feat: add Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <>HOME</>;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
 }
