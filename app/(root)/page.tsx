8655506122828d8a49e4deb8b5b5438641c9a09f
titik444 <momochi@yahoo.com>
2025-02-22 04:24:15 +0700

    feat: add Search Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -10,6 +18,8 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
