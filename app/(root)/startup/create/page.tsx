5b1d8a5975a94f9f93cd5c008cccf7af7ffc19bd
titik444 <momochi@yahoo.com>
2025-02-25 03:08:32 +0700

    feat: add Create Startup Form

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/auth";
+import StartupForm from "@/components/StartupForm";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const session = await auth();
+
+  if (!session) redirect("/");
+
+  return (
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <h1 className="heading">Submit Your Startup</h1>
+      </section>
+
+      <StartupForm />
+    </>
+  );
+}
